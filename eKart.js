38e6b9a6b19bde94704dd80fb79f34193ad558dc
piyushrana00456 <54103082+piyushrana00456@users.noreply.github.com>
2021-08-23 15:19:57 +0530

    third commit

--- eKart.js
+++ eKart.js
@@ -20,7 +20,11 @@ const connect=()=>{
 const fashionSchema=new mongoose.Schema({
     product_name:{type:String,required:true},
     product_price:{type:Number,required:true},
-    product_category:{type:String,required:true}
+    product_category:{type:String,required:true},
+    product_for:{type:String,required:true},
+    colorId:[{type :mongoose.Schema.Types.ObjectId,
+    ref:"color",
+       required:true}]
 },{
     versionKey:false
 })
@@ -30,12 +34,68 @@ const Fashion=mongoose.model("fashion",fashionSchema);
 const accessorySchema=new mongoose.Schema({
     product_name:{type:String,required:true},
     product_price:{type:Number,required:true},
+    product_for:{type:String,required:true}
+},{
+    versionKey:false
 })
 
 const Accessory=mongoose.model("accessory",accessorySchema);
 
+const colorSchema=new mongoose.Schema({
+   color:{type:String}  
+},{
+    versionKey:false
+})
+
+const Color=mongoose.model("color",colorSchema);
+
+//-------------------------------------------------CRUD-- for Faishon-------------------------------------------------------
+
+app.post("/faishon",async function(req,res){
+    try{
+        const fashion=await Faishon.create(req.body)
+        return res.status(200).send(fashion);
+    }
+    catch(err){
+        return res.status(400).send(err.message)
+    }
+})
+
+app.get("/fashion",async function(req,res){
+    try{
+        const fashion=await Faishon.find().lean().exec()
+        return res.status(200).send(fashion)
+    } 
+    catch(err){
+        return res.status(400).send(err.message)
+    }
+ 
+})
 
 
+//---------------------------------------------------------CRUD for color----------------------------------------------------------------
+
+app.post("/colors",async function(req,res){
+    try{
+        const color=await Color.create(req.body)
+        return res.status(200).send(color)
+    }
+    catch(err){
+        return res.status(400).send(err.message)
+    }
+})
 
 
+app.get("/colors",async function(req,res){
+    try{
+        const color=await Color.find().lean().exec()
+        return res.status(200).send(color)
+    }catch(err){
+        return res.status(400).send(err.message)
+    }
+})
 
+app.listen(2999,async()=>{
+   await connect();
+   console.log("I'm listening to port 2999");
+})
